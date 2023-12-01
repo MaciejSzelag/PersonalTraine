45468108c588958cd39516eb1c0bdff2205490f1
MaciejSzelag <szelag.maciej@gmail.com>
2023-12-01 23:21:06 +0000

    hamburger, menu-slide, blured content and locked body when menu active

--- js/script.js
+++ js/script.js
@@ -24,9 +24,59 @@ function paralax() {
 const socialMedia = document.getElementById("social-media")
 function showSW() {
     let scrolled = scrolling();
-    if (scrolled >= 20) {
+    if (scrolled >= 30) {
         socialMedia.style.setProperty("right", '0')
     } else {
-        socialMedia.style.setProperty("right", '-5%')
+        socialMedia.style.setProperty("right", '-20%')
     }
 }
+
+const menuBars = document.getElementById("barsWrap");
+const menuMobile = document.getElementById("menu-mobile");
+const bodyMenu = document.querySelector("body")
+const btns_a = document.querySelectorAll(".btn-a")
+const main = document.querySelector("main")
+let menuActive = true;
+menuBars.addEventListener("click", function () {
+    menuBars.classList.add("bars-wrap-active");
+    if (menuActive) {
+        menuBars.classList.add("bars-wrap-active");
+
+        menuMobile.classList.add("nav-container-active");
+
+        bodyMenu.classList.add("body-active-menu");
+        main.classList.add("main-menu-acive");
+
+        menuActive = !menuActive
+    } else if (!menuActive) {
+        menuBars.classList.remove("bars-wrap-active");
+        setTimeout(function () {
+            menuMobile.classList.remove("nav-container-active");
+            bodyMenu.classList.remove("body-active-menu");
+            main.classList.remove("main-menu-acive");
+        }, 500)
+
+        menuActive = !menuActive
+    }
+
+
+    console.log(menuActive)
+})
+btns_a.forEach((element) => {
+
+    element.addEventListener("click", function () {
+        menuBars.classList.remove("bars-wrap-active");
+
+        menuMobile.classList.remove("nav-container-active");
+        bodyMenu.classList.remove("body-active-menu");
+        main.classList.remove("main-menu-acive");
+
+        menuActive = !menuActive
+        console.log(menuActive)
+    })
+
+})
+
+
+
+
