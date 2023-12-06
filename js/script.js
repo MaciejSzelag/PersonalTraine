2442a05352c01844d95a2b687d2e6e10066ba236
MaciejSzelag <szelag.maciej@gmail.com>
2023-12-06 23:32:33 +0000

    media- header, nav, some main new images, javascript change nav's color  on visible section

--- js/script.js
+++ js/script.js
@@ -1,10 +1,7 @@
-window.onscroll = function () { progressBar(); paralax(); showSW() };
+window.addEventListener('load', handleScroll);
+window.onscroll = function () { progressBar(); paralax(); showSW(); handleScroll() };
 
-
-
-
-
-
+//behaving on scroll
 const scrolling = function () {
     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
@@ -19,7 +16,7 @@ function progressBar() {
 const headerImg = document.querySelector(".img-wrap")
 function paralax() {
     let scrolled = scrolling();
-    headerImg.style.setProperty(`transform`, ` translateY(${scrolled * 2}%)`)
+    headerImg.style.setProperty(`transform`, ` translateY(${scrolled * 1.5}%)`)
 }
 const socialMedia = document.getElementById("social-media")
 function showSW() {
@@ -30,7 +27,6 @@ function showSW() {
         socialMedia.style.setProperty("right", '-20%')
     }
 }
-
 const menuBars = document.getElementById("barsWrap");
 const menuMobile = document.getElementById("menu-mobile");
 const bodyMenu = document.querySelector("body")
@@ -41,9 +37,7 @@ menuBars.addEventListener("click", function () {
     menuBars.classList.add("bars-wrap-active");
     if (menuActive) {
         menuBars.classList.add("bars-wrap-active");
-
         menuMobile.classList.add("nav-container-active");
-
         bodyMenu.classList.add("body-active-menu");
         main.classList.add("main-menu-acive");
 
@@ -55,28 +49,48 @@ menuBars.addEventListener("click", function () {
             bodyMenu.classList.remove("body-active-menu");
             main.classList.remove("main-menu-acive");
         }, 500)
-
         menuActive = !menuActive
     }
 
-
-    console.log(menuActive)
 })
 btns_a.forEach((element) => {
-
     element.addEventListener("click", function () {
         menuBars.classList.remove("bars-wrap-active");
-
         menuMobile.classList.remove("nav-container-active");
         bodyMenu.classList.remove("body-active-menu");
         main.classList.remove("main-menu-acive");
-
         menuActive = !menuActive
         console.log(menuActive)
     })
 
 })
 
+function handleScroll() {
+    const nav_a_btns = document.querySelectorAll(".nav-section ul li");
+    const sections = document.querySelectorAll(".section-main");
+    const windowHeight = window.innerHeight;
+    let myScroll = window.scrollY + windowHeight / 2; // Przesunięcie o połowę wysokości okna
 
+    sections.forEach((section, index) => {
+        const sectionOffsetTop = section.offsetTop; //odleglosc od poczatku documenyu
+        const sectionHeight = section.clientHeight;//wysokosc elementu
+        const sectionBottom = sectionOffsetTop + sectionHeight;
+
+        // Sprawdź, czy dolna krawędź okna przekroczyła sekcję
+        if (myScroll >= sectionOffsetTop && myScroll <= sectionBottom) {
+            if (section.hasAttribute('id')) {
+                const sectionId = section.id;
+                if (sectionId) {
+                    // Zmiana koloru przycisku nawigacyjnego
+                    nav_a_btns.forEach((btn, btnIndex) => {
+                        const shouldBeActive = index === btnIndex;
+                        btn.classList.toggle('li-active', shouldBeActive);
+                        // console.log(`Button ${btnIndex} should be active: ${shouldBeActive}`)
+                    });
+                }
+            }
+        }
+    });
+}
 
 
